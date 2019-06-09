b058980293f3bf8a3d623e9cc3c73ec5256b844f
hirauchg <gentahirauchi@gmail.com>
2019-06-09 10:18:32 +0900

    音声認識機能の実装

--- app/src/main/java/com/hirauchi/voicememo/fragment/MainFragment.kt
+++ app/src/main/java/com/hirauchi/voicememo/fragment/MainFragment.kt
@@ -1,16 +1,93 @@
 package com.hirauchi.voicememo.fragment
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.hirauchi.voicememo.ui.MainFragmentUI
 import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.sdk27.coroutines.onClick
+import java.util.*
 
 class MainFragment : Fragment() {
 
+    private val mUI = MainFragmentUI()
+    private val REQUEST_CODE = 100
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return MainFragmentUI().createView(AnkoContext.create(inflater.context, this, false))
+        return mUI.createView(AnkoContext.create(inflater.context, this, false))
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mUI.apply {
+            mRecordButton.onClick {
+                startRecord()
+            }
+
+            mSaveButton.onClick {
+                saveRecord()
+            }
+
+            mDeleteButton.onClick {
+                deleteRecord()
+            }
+        }
+    }
+
+    private fun startRecord() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.JAPAN.toString())
+            putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 100)
+        }
+
+        startActivityForResult(intent, REQUEST_CODE)
+    }
+
+    private fun saveRecord() {
+        mUI.mText.text.clear()
+        changeView(false)
+
+        // TODO
+    }
+
+    private fun deleteRecord() {
+        mUI.mText.text.clear()
+        changeView(false)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            val candidates = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            if (candidates.isNotEmpty()) {
+                changeView(true)
+
+                var text = mUI.mText.text.toString()
+                if (text.trim().isNotEmpty()) {
+                    text += "\n"
+                }
+                text += candidates.first()
+                mUI.mText.setText(text)
+            }
+        }
+    }
+
+    private fun changeView(isVisible: Boolean) {
+        if (isVisible) {
+            mUI.mText.visibility = View.VISIBLE
+            mUI.mSaveWrapper.visibility = View.VISIBLE
+            mUI.mDeleteWrapper.visibility = View.VISIBLE
+        } else {
+            mUI.mText.visibility = View.GONE
+            mUI.mSaveWrapper.visibility = View.GONE
+            mUI.mDeleteWrapper.visibility = View.GONE
+        }
     }
 }
--- app/src/main/java/com/hirauchi/voicememo/ui/MainFragmentUI.kt
+++ app/src/main/java/com/hirauchi/voicememo/ui/MainFragmentUI.kt
@@ -1,15 +1,95 @@
 package com.hirauchi.voicememo.ui
 
+import android.annotation.SuppressLint
+import android.support.v4.content.ContextCompat
+import android.view.View
+import android.widget.*
+import com.hirauchi.voicememo.R
 import com.hirauchi.voicememo.fragment.MainFragment
-import org.jetbrains.anko.AnkoComponent
-import org.jetbrains.anko.AnkoContext
-import org.jetbrains.anko.textView
-import org.jetbrains.anko.verticalLayout
+import org.jetbrains.anko.*
 
 class MainFragmentUI : AnkoComponent<MainFragment> {
+
+    lateinit var mText: EditText
+    lateinit var mRecordButton: ImageButton
+    lateinit var mSaveWrapper: RelativeLayout
+    lateinit var mSaveButton: ImageButton
+    lateinit var mDeleteWrapper: RelativeLayout
+    lateinit var mDeleteButton: ImageButton
+
     override fun createView(ui: AnkoContext<MainFragment>) = with(ui) {
-        verticalLayout {
-            textView("Hello World!!")
+        relativeLayout {
+            mText = editText {
+                visibility = View.GONE
+                maxLines = 15
+            }.lparams(width = matchParent, height = wrapContent) {
+                topMargin = dip(16)
+                horizontalMargin = dip(16)
+                alignParentTop()
+            }
+
+            linearLayout {
+                mSaveWrapper = relativeLayout {
+                    visibility = View.GONE
+
+                    mSaveButton = imageButton(R.drawable.ic_save) {
+                        padding = dip(24)
+                        scaleType = ImageView.ScaleType.FIT_CENTER
+                        background = ContextCompat.getDrawable(ctx, R.drawable.radius_button)
+                        elevation = dip(4).toFloat()
+                    }.lparams(width = dip(72), height = dip(72)) {
+                        centerHorizontally()
+                    }
+
+                    textView(R.string.main_btn_save) {
+                        textSize = 16F
+                    }.lparams {
+                        centerHorizontally()
+                        alignParentBottom()
+                    }
+                }.lparams(width = 0, height = matchParent, weight = 2F)
+
+                relativeLayout {
+                    mRecordButton = imageButton(R.drawable.ic_mic) {
+                        padding = dip(24)
+                        scaleType = ImageView.ScaleType.FIT_CENTER
+                        background = ContextCompat.getDrawable(ctx, R.drawable.radius_button)
+                        elevation = dip(4).toFloat()
+                    }.lparams(width = dip(72), height = dip(72)) {
+                        centerHorizontally()
+                    }
+
+                    textView(R.string.main_btn_record) {
+                        textSize = 16F
+                    }.lparams {
+                        centerHorizontally()
+                        alignParentBottom()
+                    }
+                }.lparams(width = 0, height = matchParent, weight = 1F)
+
+                mDeleteWrapper = relativeLayout {
+                    visibility = View.GONE
+
+                    mDeleteButton = imageButton(R.drawable.ic_delete) {
+                        padding = dip(24)
+                        scaleType = ImageView.ScaleType.FIT_CENTER
+                        background = ContextCompat.getDrawable(ctx, R.drawable.radius_button)
+                        elevation = dip(4).toFloat()
+                    }.lparams(width = dip(72), height = dip(72)) {
+                        centerHorizontally()
+                    }
+
+                    textView(R.string.main_btn_delete) {
+                        textSize = 16F
+                    }.lparams {
+                        centerHorizontally()
+                        alignParentBottom()
+                    }
+                }.lparams(width = 0, height = matchParent, weight = 2F)
+            }.lparams(width = matchParent, height = dip(100)) {
+                alignParentBottom()
+                bottomMargin = dip(100)
+            }
         }
     }
 }
